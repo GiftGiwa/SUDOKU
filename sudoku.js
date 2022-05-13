00d5fe9025f0b806d16d9be70ade2f6fbebcc034
GiftGiwa <gdg2131@columbia.edu>
2022-05-13 02:43:11 -0400

    progress on implementation of puzzles into grid of input fields + updating of array with number inputs
    also restricted inputs to numbers only

--- sudoku.js
+++ sudoku.js
@@ -1,53 +1,45 @@
-let puzzle = [...Array(9)].map(e => Array(9)); //array of numbers to be filled as values are typed im
+let puzzle = [...Array(9)].map(e => Array(9)); //array of numbers to be filled as values are typed in
 
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
-let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"] //labels for ROWS of puzzle
+let letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i"] //part of labels for ROWS of puzzle
+
 const inputs = document.querySelectorAll('.buttons')  
 inputs.forEach( function (input) {
     input.addEventListener('input', (e) => {  
         let value = Number(e.target.value)
-        if (!numbers.includes(value)) //check if input is a NUMBER
-            alert("Please type numbers only.") //if input is not a NUMBER, notify user to type in a number
-        //console.log(e.target.id)
-        assign(e)  
+        inputAssign(e)  
     });
 })
-//let test_puzzles = []
-/*test_puzzles.push( [[8,3,5,4,1,6,9,2,7],
-                    [2,9,6,8,5,7,4,3,1],
-                    [4,1,7,2,9,3,6,5,8],
-                    [5,6,9,1,3,4,7,8,2],
-                    [1,2,3,6,7,8,5,4,9],
-                    [7,4,8,5,2,9,1,6,3],
-                    [6,5,2,7,8,1,3,9,4],
-                    [9,8,1,3,4,5,2,7,6],
-                    [3,7,4,9,6,2,8,1,5]] )*/
 
 let test_puzzles = []
 test_puzzles.push("[004300209005009001070060043006002087190007400050083000600000105003508690042910300]")
 //numbers.push(040100050107003960520008000000000017000906800803050620090060543600080700250097100)
 //numbers.push(600120384008459072000006005000264030070080006940003000310000050089700000502000190)
 //numbers.push(497200000100400005000016098620300040300900000001072600002005870000600004530097061)
+
 test_puzzles[0] = test_puzzles[0].substring(1, test_puzzles[0].length - 2)
-console.log(test_puzzles[0])
-let test_array = [...Array(9)].map(e => Array(9)) //testing assigning unsolved numbers to 2x2 array from excel sheet
 
-//document.getElementById("cantidadCopias").disabled = true; //<-- that's for later
+initialize() //when the "generate" button is pressed, this function is called
 
-const check = document.getElementById("check")
-check.addEventListener('click', (e) => { //events when "check" button is clicked
-    if (checkEntry(puzzle))
-        alert("RIGHT")
-    else   
-        alert("WRONG")
-})
+//initialize(Math.random(0, *number of puzzles in file*))
 
-const generate = document.getElementById("generate")
-generate.addEventListener('click', (e) => { //events when "generate" button is clicked
-    
-})
+//ADDING UNSOLVED SUDOKU TO PAGE
+function initialize() {
+    let count = 0
+    for (let i = 0; i < puzzle.length; i++) {
+        for (let j = 0; j < puzzle[0].length; j++) {
+            let num = Number(test_puzzles[0/**/].charAt(count))
+            if (num != 0)  {
+                document.getElementById(letters[i] + (j + 1)).value = num
+                document.getElementById(letters[i] + (j + 1)).disabled = true
+                document.getElementById(letters[i] + (j + 1)).classList.remove("has_hover")
+                puzzleAssign(document.getElementById(letters[i] + (j + 1)))
+            }
+            count++
+        }
+    }
+}
 
-function assign(N) { //assigns values to a 2x2 array corresponding to the inputted numbers
+function inputAssign(N) { //assigns values to a 2x2 array corresponding to the numbers inputted by user
     let ID = String(N.target.id)
     let coordinate1 = letters.indexOf(ID.substring(0, 1))
     let coordinate2 = Number(ID.substring(1)) - 1
@@ -60,11 +52,28 @@ function assign(N) { //assigns values to a 2x2 array corresponding to the inputt
     }
 }
 
-function checkEntry(grid) {
-    if (isValidAcrossRows(grid) && isValidAcrossColumns(grid)) //check if conditions for valid sudoku solution are fulfilled
-        return true
-    return false
+function puzzleAssign(N) {
+    let coordinate1 = letters.indexOf(N.id.substring(0, 1))
+    let coordinate2 = Number(N.id.substring(1)) - 1
+    puzzle[coordinate1][coordinate2] = Number(N.value)
 }
+
+//CHECK BUTTON
+
+document.getElementById("check").addEventListener('click', (e) => { //events when "check" button is clicked
+    if (checkEntry(puzzle))
+        alert("RIGHT") // to be updated
+    else   
+        alert("WRONG") // to be updated
+})
+
+//GENERATE BUTTON
+
+document.getElementById("generate").addEventListener('click', (e) => { //events when "generate" button is clicked
+    alert("test") // to be updated
+})
+
+//CHECKING ENTRIES
 
 function isValidAcrossRows(grid) { //checks that each number is unique across every row
     for (let i = 0; i < grid.length; i++) {
@@ -92,3 +101,6 @@ function isValidAcrossColumns(grid) { //checks that each number is unique across
     return true
 }
 
+function checkEntry(grid) {
+    return (isValidAcrossRows(grid) && isValidAcrossColumns(grid)) //check if conditions for valid sudoku solution are fulfilled
+}
